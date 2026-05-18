4fb6c85b4277f05770310536bb3b5f1c00a8623f
Joao Moreira <up201905212@up.pt>
2026-05-18 11:33:45 +0100

    Added support to webapp for all models

--- models/decisiontree.py
+++ models/decisiontree.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 import joblib
-
 import os
+
 df = pd.read_csv("data/dataset1.csv")
 
 X = df.drop(columns=["nome", "risco_lesao"])
@@ -21,7 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
     test_size=0.3,
-    random_state=42,
+    random_state=40,
     stratify=y
 )
 
@@ -38,30 +38,8 @@ print(classification_report(y_test, pred))
 print("\nConfusion matrix:")
 print(confusion_matrix(y_test, pred))
 
-# guardar modelo
-
+# Guardar modelo
 os.makedirs("models", exist_ok=True)
-joblib.dump(
-    model,
-    "models/injury_model_decisiontree.pkl"
-)
+joblib.dump(model, "models/dt_injury_model.pkl")
 
-print(
-    "\nModelo guardado."
-)
-
-'''
-
-1. Lê uma tabela com jogadores - 
-2. Separa os dados em:
-   - características dos jogadores
-   - risco de lesão
-3. Dá parte dos dados ao algoritmo para ele aprender
-4. Guarda outra parte para testar
-5. Treina a árvore de decisão
-6. Pede à árvore para prever o risco
-7. Compara as previsões com as respostas verdadeiras
-8. Mostra se o modelo acertou muito ou pouco
-9. Guarda o modelo para usar depois na app
-
-'''
+print("\nModelo guardado.")
--- models/randomforest.py
+++ models/randomforest.py
@@ -30,7 +30,7 @@ y = df["risco_lesao"]
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
-    test_size=0.2,
+    test_size=0.3,
     random_state=42
 )
 
